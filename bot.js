2cea570b02a3083dff534e8aeacecd3e2be83262
domheri <47629857+domheri@users.noreply.github.com>
2019-03-10 12:19:09 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -231,6 +231,8 @@ client.on("message", message => {
 ❖=emojilist ~ يعرض لك كل الايموجيات الي بالسيرفر
 ❖=say ~ يكرر الكلام الي تكتبو
 ❖=image ~ صورة السيرفر
+❖=credit ~ يوريك كم الكريديت حقتك
+❖=daily ~ يسوي لك سحب فلوس
 ❖=members ~ يعرض لك عدد كل حالات الاشخاص وعدد البوتات وعدد الاشخاص
 ❖=id ~ معلومات عنك
 ❖=profile ~ عرض بروفايل حقك
@@ -2301,6 +2303,18 @@ if (!message.channel.guild) return;
  }
  });
 
+client.on('message' , message => {
+var prefix = "="
+
+if (message.author.bot) return;
+if (message.content.startsWith(prefix + "credit")) {
+if (!message.channel.guild) return;
+                        ctx.font = "bold 12px kathen" // نوع الخط وحجمه
+                        ctx.fontSize = '10px'; // عرض الخط
+                        ctx.fillStyle = "#f1f1f1" // لون الخط
+                        ctx.textAlign = "center"; // محاذا ة النص
+                        ctx.fillText(`$${profile[getvalueof.id].credits}`, 81, 159) // احداثيات المصاري
+
 client.on('message', message => {
 	var prefix = "="
   if (message.author.x5bz) return;
@@ -2339,6 +2353,55 @@ client.on('message', message => {
   })
 }
 });
+
+client.on('message' , message => {
+var prefix = "="
+
+if (message.author.bot) return;
+if (message.content.startsWith(prefix + "daily")) {
+  if(profile[message.author.id].lastDaily != moment().format('day')) {
+    profile[message.author.id].lastDaily = moment().format('day')
+    profile[message.author.id].credits += 200
+     message.channel.send(`**${message.author.username} you collect your \`200\` :dollar: daily pounds**`)
+} else {
+    message.channel.send(`**:stopwatch: | ${message.author.username}, your daily :yen: credits refreshes ${moment().endOf('day').fromNow()}**`)
+}
+  }
+
+ 
+ let cont = message.content.slice(prefix.length).split(" ");
+let args = cont.slice(1);
+let sender = message.author
+if(message.content.startsWith(prefix + 'trans')) {
+          if (!args[0]) {
+            message.channel.send(`**Usage: ${prefix}trans @someone amount**`);
+         return;
+           }
+        // We should also make sure that args[0] is a number
+        if (isNaN(args[0])) {
+            message.channel.send(`**Usage: ${prefix}trans @someone amount**`);
+            return; // Remember to return if you are sending an error message! So the rest of the code doesn't run.
+             }
+            let defineduser = '';
+            let firstMentioned = message.mentions.users.first();
+            defineduser = (firstMentioned)
+            if (!defineduser) return message.channel.send(`**Usage: ${prefix}trans @someone amount**`);
+            var mentionned = message.mentions.users.first();
+if (!profile[sender.id]) profile[sender.id] = {}
+if (!profile[sender.id].credits) profile[sender.id].credits = 200;
+fs.writeFile('profile.json', JSON.stringify(profile), (err) => {
+if (err) console.error(err);
+})
+      var mando = message.mentions.users.id;
+      if  (!profile[defineduser.id]) profile[defineduser.id] = {}
+      if (!profile[defineduser.id].credits) profile[defineduser.id].credits = 200;
+      profile[defineduser.id].credits += (+args[0]);
+      profile[sender.id].credits += (-args[0]);
+      let mariam = message.author.username
+message.channel.send(`**:moneybag: | ${message.author.username}, has transferrerd ` + "`" + args[0] + "$` to " + `<@${defineduser.id}>**`)
+}
+ 
+      });
 
 client.on('message', message => {
 	var prefix = "="
